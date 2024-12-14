6d9e92dfeaf43616c171172a8f490b1d9a4ba8d7
Jon Ramos <134915416+ItsDevJon@users.noreply.github.com>
2024-12-14 18:33:44 +0100

    Initial project setup

---
+++ src/main/java/org/spinescope/diagnosisapi/SpineScopeDiagnosisApiApplication.java
@@ -0,0 +1,13 @@
+package org.spinescope.diagnosisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpineScopeDiagnosisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpineScopeDiagnosisApiApplication.class, args);
+    }
+
+}
