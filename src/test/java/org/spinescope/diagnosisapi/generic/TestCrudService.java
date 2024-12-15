94772c070c5c24b272d3f4ee9e6cdea4dc509774
Jon Ramos <134915416+ItsDevJon@users.noreply.github.com>
2024-12-15 14:42:24 +0100

    Add unit testing

---
+++ src/test/java/org/spinescope/diagnosisapi/generic/TestCrudService.java
@@ -0,0 +1,11 @@
+package org.spinescope.diagnosisapi.generic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public class TestCrudService extends AbstractCrudService<String, Long, CrudRepository<String, Long>> {
+
+    public TestCrudService(CrudRepository<String, Long> repository) {
+        super(repository);
+    }
+
+}
