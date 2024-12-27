cdbe5aba37690a531404a99127cd088bf0552291
Jon Ramos <134915416+ItsDevJon@users.noreply.github.com>
2024-12-27 23:28:40 +0100

    Add User repository

---
+++ src/main/java/org/spinescope/diagnosisapi/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package org.spinescope.diagnosisapi.domain.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
